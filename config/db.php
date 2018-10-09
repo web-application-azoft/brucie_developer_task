0d2a29980e9cd7675d6ab440e6a04169dc8ef02a
rkasatin <kasatkin@azoft.com>
2018-10-09 13:09:33 +0700

    QuickFix: db config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-basic.local',
+    'username' => 'yii2-basic.local',
+    'password' => 'yii2-basic.local',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
